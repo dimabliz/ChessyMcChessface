703c0fabd0a9edf424ee3132f991ce7ef2cecb32
Dmitriy Bliznyuk <dimabliz@gmail.com>
2017-06-25 22:36:57 -0700

    changed abstract class

--- Pieces/AbstractPiece.java
+++ Pieces/AbstractPiece.java
@@ -4,13 +4,33 @@ public abstract class AbstractPiece implements Piece {
 	
 	private int pointValue;
 	
-	protected AbstractPiece(PieceType pt, boolean isTheWhite) {
-		isWhite = isTheWhite;
-		switch(pt) {
-			case PieceType.PAWN:
-				
-			case PieceType.ROOK:
-				
+	private int myX;
+	
+	private int myY;
+	
+	private String pieceName;
+	
+//	protected AbstractPiece(PieceType pt, boolean isTheWhite) {
+//		isWhite = isTheWhite;
+//		switch(pt) {
+//			case PieceType.PAWN:
+//				
+//			case PieceType.ROOK:
+//				
+//		}
+//	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(pieceName);
+		if(isWhite) {
+			sb.append(", white, ");
+		} else {
+			sb.append(", black, {");
 		}
+		sb.append(myX);
+		sb.append(", ");
+		sb.append(myY);
+		sb.append("}\n");
 	}
 }
--- Pieces/Piece.java
+++ Pieces/Piece.java
@@ -3,6 +3,12 @@ public interface Piece {
   
 	boolean isWhite();
 	
+	int getX();
+	
+	int getY();
+	
+	void moveTo();
+	
 	int getPointValue();
 	
 	String toString();
