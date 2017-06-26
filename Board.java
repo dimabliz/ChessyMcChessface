d05906211c3477871c696c4cc3a1323c3ae450f9
Dmitriy Bliznyuk <dimabliz@gmail.com>
2017-06-25 22:02:31 -0700

    made board class

---
+++ Board.java
@@ -0,0 +1,16 @@
+public class Board {
+	
+	Piece[][] myBoard;
+	
+	public Board() {
+		myBoard = new Piece[8][8];
+	}
+	
+	public void initializePieces() {
+		
+	}
+	
+	public String toString() {
+		
+	}
+}
