5a40a1f154b75b6bdac1e6afd489bead640248bc
Dmitriy Bliznyuk <dimabliz@gmail.com>
2017-06-27 16:34:59 -0700

    added get availbale moves to pawn class

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -1,5 +1,6 @@
 package Pieces;
 import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
 
 import Enums.PieceColor;
@@ -28,7 +29,35 @@ public class Pawn extends AbstractPiece {
 	 * @return 
 	 */
 	public List<Point> getAvailableMoves() {
-		return null;
+		List<Point> moves = new ArrayList<Point>();
+		
+		//white pawns move up the board
+		if (myColor == PieceColor.White) {
+			if (myLocation.y-1 > 0) { // one square up
+				moves.add(new Point(myLocation.x, myLocation.y-1));
+			} else if (myLocation.y-2 > 0) { //two squares up
+				moves.add(new Point(myLocation.x, myLocation.y-2));
+			} else if (myLocation.y-1 > 0 && myLocation.x-1 > 0) { //take left
+				moves.add(new Point(myLocation.x-1, myLocation.y-1));
+			} else if (myLocation.y-1 > 0 && myLocation.x+1 < 8) { //take right
+				moves.add(new Point(myLocation.x+1, myLocation.y-1));
+			}
+		} 
+		
+		//black pawns move down the board
+		else {
+			if (myLocation.y+1 < 8) { // one square down
+				moves.add(new Point(myLocation.x, myLocation.y+1));
+			} else if (myLocation.y+2 < 8) { //two squares down
+				moves.add(new Point(myLocation.x, myLocation.y+2));
+			} else if (myLocation.y+1 > 0 && myLocation.x-1 > 0) { //take right
+				moves.add(new Point(myLocation.x-1, myLocation.y+1));
+			} else if (myLocation.y+1 > 0 && myLocation.x+1 < 8) { //take left
+				moves.add(new Point(myLocation.x+1, myLocation.y+1));
+			}
+		}
+		
+		return moves;
 	}
 	
 	/**
