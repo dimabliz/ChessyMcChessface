a1bedd2b3925956f07141440c5a10e3656da892b
Dmitriy Bliznyuk <dimabliz@gmail.com>
2017-06-27 16:54:34 -0700

    set up the piece Testing class

--- src/chess/PieceTesting.java
+++ src/chess/PieceTesting.java
@@ -4,14 +4,28 @@ import Pieces.*;
 
 public class PieceTesting {
 
-	String[][] myTestBoard = new String[8][8];
+	public static String[][] myTestBoard = new String[8][8];
 	
 	public static void main(String[] args) {
 		
-	
+		printBoard();
 
 	}
 	
-	
+	/**
+	 * Prints the current state of the board.
+	 */
+	public static void printBoard() {
+		for(String[] row : myTestBoard) {
+			System.out.print("|");
+			for(String piece : row) {
+				if (piece == null)
+					System.out.print("  |");
+				else
+					System.out.print(piece + "|");
+			}
+			System.out.println();
+		}
+	}
 
 }
