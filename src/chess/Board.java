cc219fa7306b8635df33fe62519bc3c1daf93179
Dmitriy Bliznyuk <dimabliz@gmail.com>
2017-06-27 14:46:25 -0700

    added the board class

---
+++ src/chess/Board.java
@@ -0,0 +1,21 @@
+package chess;
+
+import Pieces.Piece;
+
+public class Board {
+	
+	Piece[][] myBoard;
+	
+	public Board() {
+		myBoard = new Piece[8][8];
+	}
+	
+	public void initializePieces() {
+		
+	}
+	
+	public void printBoard() {
+		
+	}
+}
+
