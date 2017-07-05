4d1d7576df480a1e665c25a13d567ab5e580c468
Dmitriy Bliznyuk <dimabliz@gmail.com>
2017-07-05 01:17:48 -0700

    trying to set up a working game in main

--- src/chess/Main.java
+++ src/chess/Main.java
@@ -1,11 +1,32 @@
 package chess;
 
+import java.util.Scanner;
+
 public class Main {
 	
   public static void main(String[] args) {
   
-	  Board board = new Board();
-	  board.initializePieces();
-	  board.printBoard();
+//	  Board board = new Board();
+//	  board.initializePieces();
+//	  board.printBoard();
+	  
+	  Scanner scan = new Scanner(System.in);
+	  String word = "a";
+	  boolean turn = true;
+	  
+	  while(word.charAt(0) != 'q') {
+		  if (turn) {
+			  System.out.print("Enter a move for WHITE: ");
+			  word = scan.nextLine();
+			  System.out.println(word);
+		  } else {
+			  System.out.print("Enter a move for BLACK: ");
+			  word = scan.nextLine();
+			  System.out.println(word);
+		  }
+		  turn = !turn;
+	  }
+	  
+	  scan.close();
   }
 }
