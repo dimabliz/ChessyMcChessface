3712748f7883f880ceda96ae22b31f95d301725b
Dmitriy Bliznyuk <dimabliz@uw.edu>
2018-01-03 17:18:37 -0800

    AI will now checkmate you if it sees the opportunity

--- src/AI/AI.java
+++ src/AI/AI.java
@@ -55,6 +55,7 @@ public class AI {
                 new Point((int) finalMove.to.getY(), (int)finalMove.to.getX()));
     }
 
+    // Looking to take the most valuable ratio exchange
     public void makeMoveLevel1(PieceColor turn) {
         List<Move> allPossibleMoves = getAllPossibleMoves(turn);
 
@@ -85,10 +86,39 @@ public class AI {
         Random randy = new Random();
         Move finalMove = sameRatioMoves.get(randy.nextInt(sameRatioMoves.size()));
 
-        System.out.println("Ration is " + maxRatio);
+        System.out.println("Ratio is " + maxRatio);
 
         myGameBoard.move(new Point((int) finalMove.from.getY(), (int)finalMove.from.getX()),
                 new Point((int) finalMove.to.getY(), (int)finalMove.to.getX()));
+    }
+
+    // Avoiding a checkmate on next move and looking for a checkmate current move.
+    public void makeMoveLevel2(PieceColor turn) {
+        List<Move> allPossibleMoves = getAllPossibleMoves(turn);
+        boolean level2Used = false;
+
+        // Code below is searching if any of the moves will checkmate the opponent
+        Move checkmatingMove = null;
+        for (Move move : allPossibleMoves) {
+            myGameBoard.simpleMove(new Point((int) move.from.getY(), (int)move.from.getX()),
+                    new Point((int) move.to.getY(), (int)move.to.getX()), true);
+            myGameBoard.checkForCheckMate(myGameBoard.getLastPieceMoved().getColor() == PieceColor.White ? PieceColor.Black : PieceColor.White);
+            if (myGameBoard.getCountPossibleMoves() == 0) {
+                checkmatingMove = move;
+            }
+            myGameBoard.simpleMove(new Point((int) move.to.getY(), (int)move.to.getX()),
+                    new Point((int) move.from.getY(), (int)move.from.getX()), false);
+        }
+
+        if (checkmatingMove != null) {
+            myGameBoard.move(new Point((int) checkmatingMove.from.getY(), (int)checkmatingMove.from.getX()),
+                    new Point((int) checkmatingMove.to.getY(), (int)checkmatingMove.to.getX()));
+            level2Used = true;
+        }
+
+        // Code below is avoiding any checkmates that the opponent has in store for them the next move.
+        
+
     }
 
 
